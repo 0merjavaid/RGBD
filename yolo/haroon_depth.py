42fb87e8809ceff4c1875eacc08e4d0817bf75df
0merjavaid <14besemjavaid@seecs.edu.pk>
2018-10-30 17:55:30 +0500

    Haroon:Final commit 30 Oct

--- yolo/haroon_depth.py
+++ yolo/haroon_depth.py
@@ -28,23 +28,14 @@ try:
             continue
 
         color_image, depth_image = rgbd_to_numpy(color_frame, aligned_depth_frame)
-        
-
-        print(color_image.shape)
-        print(depth_image.shape)
-        print('Hello')
-        color_image_1 = color_image.copy()
-
-        #rgb_image = cv2.cvtColor(color_image, cv2.COLOR_BGR2RGB)
-
+        color_image_ioi = color_image.copy()
         
         # Apply YOLO to this image and get bounding boxes
         hands, items= YOLO.get_item_of_interest(color_image)
         result = run(hands,items)
         
-        # a = items[np.unravel_index(result.argmax(), result.shape)[1]]
+        # get hand centroid
         hand_centroids = []
-
         if len(hands) > 0 and result.shape[1] != 0:
             hand_of_interest = hands[np.unravel_index(result.argmax(), result.shape)[0]]
         
@@ -60,17 +51,15 @@ try:
                                 (255,255,255),
                                 2)
             cv2.rectangle(color_image,(hand_of_interest[0],hand_of_interest[1]),(hand_of_interest[2],hand_of_interest[3]),(0,255,0),2)
-        item_centroids = []
-             
+        
+        # get item centroid
+        item_centroids = []    
         for box in items: 
             if box is not None:
                 x1, y1, x2, y2 = box
                 depth_crop = depth_image[y1:y2, x1:x2]
                 median_depth_item = np.median(depth_crop.reshape(-1))
                 item_centroids.append((median_depth_item, box))
-                #depth = depth_image[x_center, y_center]
-                #item_boxes.append(box)
-                #item_centroids.append((x_center, y_center, depth))
                 cv2.putText(color_image,'Depth:' + str(median_depth_item), 
                                 (box[0],box[1]), cv2.FONT_HERSHEY_SIMPLEX, 
                                 1,
@@ -79,7 +68,6 @@ try:
                 cv2.rectangle(color_image,(box[0],box[1]),(box[2],box[3]),(0,0,255),4)
         
         # All hands and items are detected
-        
         if (len(hand_centroids) != 0) and (len(item_centroids) != 0):
             hand_depth = hand_centroids[0][0]
             hand_box = hand_centroids[0][1]
@@ -104,9 +92,7 @@ try:
                 if depth < 150:
                     final_box = final_tuple[0]
                     print("Final Box: ", final_box)
-                    cv2.rectangle(color_image_1,(final_box[0],final_box[1]),(final_box[2], final_box[3]),(0,0,0),2)        
-        
-        #Get centroid of each bounding box
+                    cv2.rectangle(color_image_ioi,(final_box[0],final_box[1]),(final_box[2], final_box[3]),(0,0,0),2)        
 
 
         #cv2.imshow('Color',rgb_image)
@@ -115,10 +101,10 @@ try:
         cv2.imshow('image',color_image)
         cv2.waitKey(1)
 
-        #cv2.imshow('Imp',color_image_1)
+        #cv2.imshow('Imp',color_image_ioi)
         cv2.namedWindow('ioi', cv2.WINDOW_NORMAL)
         cv2.resizeWindow('ioi',1280, 720)
-        cv2.imshow('ioi',color_image_1)
+        cv2.imshow('ioi',color_image_ioi)
         cv2.waitKey(1)
 
 finally:
