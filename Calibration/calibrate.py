2795012cc76be600d88347020adba98aa6e68ab3
0merjavaid <14besemjavaid@seecs.edu.pk>
2019-03-04 14:35:32 +0500

    evaluation

--- Calibration/calibrate.py
+++ Calibration/calibrate.py
@@ -63,6 +63,39 @@ def mask_inturrept(image, indexes, box_height, box_width):
     return image
 
 
+def detect_inturrept(depth_frame, buffer_counter, buffer_depths):
+    col_intervals = 10
+    row_intervals = 2
+    cols_boundary = np.zeros((col_intervals,))
+    cols = int(848/col_intervals)
+    rows = int(480/row_intervals)
+    rows = 100
+    thres = 10
+    plane_points = [100]
+    plane_height = 5
+
+    depth_frame = (depth_frame.astype(float)*255/1000).astype("uint8")
+    depth_frame = cv2.GaussianBlur(depth_frame, (5, 5), 0)
+    cols_depths = get_regions_depth(
+        depth_frame, plane_points, plane_height, col_intervals)
+
+    if buffer_counter < 10:
+        buffer_depths = np.array(buffer_depths)
+        buffer_depths = np.concatenate(
+            (cols_depths.reshape(1, row_intervals-1, -1), buffer_depths[0:-1, ]), axis=0)
+
+    cols_boundary = np.mean(buffer_depths, 0)
+
+    diffs = abs(cols_depths-cols_boundary)
+    difference_detected = np.where(diffs > thres)
+
+    if len(difference_detected[0]) > 0:
+        return True, buffer_counter, buffer_depths
+    else:
+        return False, buffer_counter, buffer_depths
+        # mask_inturrept(color_frame, difference_detected, rows, cols)
+
+
 def plane_inturrept():
     cam_name = rs.context().devices[0].get_info(
         rs.camera_info.serial_number)
@@ -70,81 +103,61 @@ def plane_inturrept():
     camera = Realsense(cam_name, (640, 360), (848, 480), 30)
 
     buffer_counter = 0
-    f1, s1, t1 = 0, 0, 0
-    col_intervals = 10
-    row_intervals = 5
-    cols_boundary = np.zeros((col_intervals,))
-    cols = int(848/col_intervals)
-    rows = int(480/row_intervals)
-    thres = 10
-    plane_points = [rows*(i+1) for i in range(row_intervals-1)]
+
+    # [rows*(i+1) for i in range(row_intervals-1)]
+
     buffer_size = 3
-    plane_height = 5
-    buffer_depths = np.zeros((buffer_size, row_intervals-1,  col_intervals))
+
+    buffer_depths = [np.zeros((buffer_size, 1,  10)) for i in range(cams))]
     while True:
         buffer_counter += 1
-        color_frame, depth_frame = next(camera.get_frames())
-        depth_frame[depth_frame > 1000] = 1000
-        depth_frame = (depth_frame.astype(float)*255/1000).astype("uint8")
-        depth_frame = cv2.GaussianBlur(depth_frame, (5, 5), 0)
+        color_frame, depth_frame=next(camera.get_frames())
+        depth_frame[depth_frame > 1000]=1000
+        _, __, buffer_depths=detect_inturrept(
+            depth_frame, buffer_counter, buffer_depths)
+        print _
 
-        cols_depths = get_regions_depth(
-            depth_frame, plane_points, plane_height, col_intervals)
-
-        if buffer_counter < 10:
-            buffer_depths = np.array(buffer_depths)
-            buffer_depths = np.concatenate(
-                (cols_depths.reshape(1, row_intervals-1, -1), buffer_depths[0:-1, ]), axis=0)
-
-        cols_boundary = np.mean(buffer_depths, 0)
-
-        diffs = abs(cols_depths-cols_boundary)
-        difference_detected = np.where(diffs > thres)
-
-        if len(difference_detected[0]) > 0:
-            mask_inturrept(color_frame, difference_detected, rows, cols)
-        cv2.imshow("", color_frame)
-        key = cv2.waitKey(1)
-        if key == ord("q"):
-            break
-    cv2.destroyAllWindows()
+        # cv2.imshow("", color_frame)
+        # key = cv2.waitKey(1)
+        # if key == ord("q"):
+        #     break
+        # cv2.destroyAllWindows()
 
 
 def main():
 
-    points = np.loadtxt("points.txt").reshape(-1, 5)
-    median_depths = np.loadtxt("depths.txt").reshape(-1, 3)
-    cam_ids = points[:, 0]
-    points = points[:, 1:].reshape(-1, 2, 2).astype(int)
-    cam_ids = points[:, 0]
-    median_depths = median_depths[:, 1:].reshape(-1, 2).astype(int)
+    points=np.loadtxt("points.txt").reshape(-1, 5)
+    median_depths=np.loadtxt("depths.txt").reshape(-1, 3)
+    cam_ids=points[:, 0]
+    points=points[:, 1:].reshape(-1, 2, 2).astype(int)
+    cam_ids=points[:, 0]
+    median_depths=median_depths[:, 1:].reshape(-1, 2).astype(int)
 
-    ctx = rs.context()
-    devices_list = []
+    ctx=rs.context()
+    devices_list=[]
     for d in ctx.devices:
         if d.get_info(rs.camera_info.name) == "Intel RealSense D435":
             devices_list.append(d.get_info(rs.camera_info.serial_number))
     print (len(devices_list), " Cameras Connected")
     print (devices_list)
-    cameras = [Realsense(i, (640, 360), (848, 480), 30) for i in devices_list]
+    cameras=[Realsense(i, (640, 360), (848, 480), 30) for i in devices_list]
 
     for camera in cameras:
         camera.assign_cam_id()
 
     for camera in cameras:
-        point = points[camera.cam_id-1]
+        point=points[camera.cam_id-1]
 
-        print ("cam id", camera.cam_id, point)
-        calibrator = Calibrate(point)
-        median = [0, 0]
-        stored_depths = median_depths[camera.cam_id-1]
-        ret = True
+        calibrator=Calibrate(point)
+        median=[0, 0]
+        stored_depths=median_depths[camera.cam_id-1]
+        ret=True
         while ret:
 
-            color_frame, depth_frame = next(camera.get_frames())
+            color_frame, depth_frame=next(camera.get_frames())
             for i in range(len(point)):
                 # print (i)
-                median[i] = np.median(
+                median[i]=np.median(
                     depth_frame[point[i][1]-1:point[i][1]+1,
                                 point[i][0]-1:point[i][0]+1])
             # print ("median depth", median)
@@ -152,7 +165,7 @@ def main():
             calibrator.diff(stored_depths, median, 15)
             calibrator.plot_circles(color_frame)
             cv2.imshow("", color_frame)
-            key = cv2.waitKey(1)
+            key=cv2.waitKey(1)
             if key == ord("q"):
                 break
         cv2.destroyAllWindows()
